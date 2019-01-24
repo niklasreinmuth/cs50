14544975436138d98da9d30b37e9a6c896be6491
niklasreinmuth <niklas.reinmuth@googlemail.com>
2019-01-24 21:37:36 +0100

    prepared tasks

--- credit.c
+++ credit.c
@@ -1,8 +1,32 @@
-//test
-
 #include <stdio.h>
+#include <cs50.h>
+
+long check_credit_card(string);
 
 int main(void)
 {
-    
+    //get credit card number (ccn)
+    //print if ccn is a valid american express, mastercard or visa number
 }
+
+long check_credit_card(string valid)
+{    
+    long ccn = get_long("Insert credit card number:\n");
+    
+    if(ccn % 2 == 0)
+    {
+        
+    }
+    else
+    {
+        
+    }
+    
+    return valid;
+}
+//function check_card()
+//get input long
+//reject non numbers (hyphens, letters)
+//check if ccn is valid
+//return status (name of card or invalid)
+
