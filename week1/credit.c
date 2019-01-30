aa7e14d610e7a3ebbd426bf75a39d6a13406542b
niklasreinmuth <niklasreinmuth@users.noreply.github.com>
2019-01-30 06:34:27 +0100

    Update credit.c

--- week1/credit.c
+++ week1/credit.c
@@ -20,9 +20,21 @@ long get_credit_card(string prompt)
     {
         card_number = get_long("%s", prompt);
     }
-    while(card_number < 0);
+    while (card_number < 0);
     
     return card_number;
+}
+    
+int get_card_length(long card_number)
+{
+    int length = 0;
+    while (card_number > 0)
+    {
+        card_number /= 10;
+        length++;
+    }
+    
+    return length;
 }
 
 string validate_credit_card(long card_number)
@@ -31,48 +43,49 @@ string validate_credit_card(long card_number)
     string card_type = "INVALID";
     int checksum = 0;
     
-    // isolate every other rightmost digit and add to checksum
-    for(long i = 1; i <= length * 100000000000000; i *= 100)
+    // isolate every other rightmost digit and add to checksum.
+    for (long i = 1; i <= length * 100000000000000; i *= 100)
     {
         int digit = (card_number / i % 10);
         checksum += digit;
     }    
 
-    // isolate every other rightmost digit, double it and
-    // divide the values if above 10, then add those to checksum
-    for(long j = 10; j <= length * 1000000000000000; j *= 100)
+    // isolate every other (other) rightmost digit, double it and add 
+    // those to the checksum. if the doubled sum exceeds 10 however, 
+    // split the digits and add those to checksum instead.
+    for (long j = 10; j <= length * 1000000000000000; j *= 100)
     {
         int digit = (card_number / j % 10) * 2;
         checksum += (digit % 10) + (digit / 10);
     }
     
-    if(checksum % 10 == 0)
+    if (checksum % 10 == 0)
     {
-        if(length == 16)
+        if (length == 16)
         {
             int mc = card_number / 100000000000000;
-            if(mc == 51 || mc == 52 || mc == 53 || mc == 54 || mc == 55)
+            if (mc == 51 || mc == 52 || mc == 53 || mc == 54 || mc == 55)
             {
                 card_type = "MASTERCARD";
             }
-            else if(card_number / 1000000000000000 == 4)
+            else if (card_number / 1000000000000000 == 4)
             {
                 card_type = "VISA";
             }        
         }
         
-        if(length == 15)
+        if (length == 15)
         {
             int amex = card_number / 10000000000000;
-            if(amex == 34 || amex == 37)
+            if (amex == 34 || amex == 37)
             {
                 card_type = "AMEX";
             }
         }
         
-        if(length == 13)
+        if (length == 13)
         {
-            if(card_number / 1000000000000 == 4)
+            if (card_number / 1000000000000 == 4)
             {
                 card_type = "VISA";
             }
@@ -80,16 +93,4 @@ string validate_credit_card(long card_number)
     }
     
     return card_type;
-}
-    
-int get_card_length(long card_number)
-{
-    int length = 0;
-    while(card_number > 0)
-    {
-        card_number /= 10;
-        length++;
-    }
-    
-    return length;
 }
